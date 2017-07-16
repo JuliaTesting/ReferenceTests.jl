15048b1410e827aef69e6e3642110f37132e12d6
Christof Stocker <stocker.christof@gmail.com>
2017-07-17 00:25:51 +0200

    outline placeholder replacement

--- src/ReferenceTests.jl
+++ src/ReferenceTests.jl
@@ -1,5 +1,51 @@
 module ReferenceTests
 
-# package code goes here
+using FileIO
+
+export
+
+    @test_reference
+
+function replace_placeholder(expr::Expr)
+    if expr.head == :call
+        for i = 2:length(expr.args)
+            arg = expr.args[i]
+            if arg isa Expr && arg.head == :(::)
+                if arg.args[1] == :IO
+                    # show(::IO, ...)
+                    placeholder = arg.args[1]
+                    nvar = Symbol("#io")
+                    nexpr = deepcopy(expr)
+                    nexpr.args[i] = nvar
+                    nexpr = quote
+                        $nvar = IOBuffer()
+                        $nexpr
+                        readstring(seek($nvar, 0))
+                    end
+                    return nexpr
+                end
+            end
+        end
+    end
+    return expr
+end
+
+macro test_reference(filename, expr)
+    nexpr = replace_placeholder(expr)
+    :(test_reference_impl($filename, $(esc(nexpr))))
+end
+
+function extended_query(filename)
+    file, ext = splitext(filename)
+    if ext == "" || uppercase(ext) == ".txt"
+        File{format"TXT"}(filename)
+    else
+        query(filename)
+    end
+end
+
+function test_reference_impl(filename, actual)
+    println(filename, " ", actual)
+end
 
 end # module
