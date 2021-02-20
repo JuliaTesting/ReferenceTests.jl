96a6d45b7534e126e1e97aecb26693f13ddbbbfd
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-02-20 00:48:39 +0000

    disable plot test on linux

--- test/runtests.jl
+++ test/runtests.jl
@@ -122,8 +122,11 @@ end
 end
 
 @testset "Plots as PNG images" begin
-    @test_reference "references/heatmap.png" heatmap([1 0; 0 1]) by=psnr_equality(15)
-    @test_reference "references/scatter.png" scatter([(0,0),(1,0),(0,1),(1,1)],ms=8)
+    # Test disabled on linux because: https://github.com/JuliaPlots/Plots.jl/issues/2127
+    if !Sys.islinux()
+        @test_reference "references/heatmap.png" heatmap([1 0; 0 1]) by=psnr_equality(15)
+        @test_reference "references/scatter.png" scatter([(0,0),(1,0),(0,1),(1,1)], ms=8)
+    end
 end
 
 using DataFrames, CSVFiles
