5b204fb3abe587ac707f99036d189fc0ff66a120
Tim Holy <tim.holy@gmail.com>
2021-03-05 03:47:42 -0600

    Warn, not error, on precomilation failure
    Fixes #84

--- src/ReferenceTests.jl
+++ src/ReferenceTests.jl
@@ -22,10 +22,15 @@ include("equality_metrics.jl")
 include("render.jl")
 
 if Base.VERSION >= v"1.4.2" && ccall(:jl_generating_output, Cint, ()) == 1
-    @assert precompile(Tuple{Core.kwftype(typeof(test_reference)),NamedTuple{(:by,), Tuple{typeof(isequal)}},typeof(test_reference),String,Matrix{RGB{Float64}}})   # time: 0.4697069
-    @assert precompile(Tuple{Core.kwftype(typeof(test_reference)),NamedTuple{(:by,), Tuple{typeof(isequal)}},typeof(test_reference),String,Matrix{RGBA{Float64}}})   # time: 0.24671553
-    @assert precompile(Tuple{typeof(test_reference),File{DataFormat{:PNG}},Matrix{RGB{Float64}},Function,Nothing})   # time: 0.029915236
-    @assert precompile(Tuple{typeof(test_reference),File{DataFormat{:PNG}},Matrix{RGBA{Float64}},Function,Nothing})   # time: 0.013356352
+    precompile(Tuple{Core.kwftype(typeof(test_reference)),NamedTuple{(:by,), Tuple{typeof(isequal)}},typeof(test_reference),String,Matrix{RGB{Float64}}})  || @warn "Failure to precompile ReferenceTests.test_reference(String,Matrix{RGB})"
+    precompile(Tuple{Core.kwftype(typeof(test_reference)),NamedTuple{(:by,), Tuple{typeof(isequal)}},typeof(test_reference),String,Matrix{RGBA{Float64}}}) || @warn "Failure to precompile ReferenceTests.test_reference(String,Matrix{RGBA})"
+    if isdefined(FileIO, :action)
+        precompile(Tuple{typeof(test_reference),File{DataFormat{:PNG},String},Matrix{RGB{Float64}},Function,Nothing})  || @warn "Failure to precompile ReferenceTests.test_reference(File{format\"PNG\",String},Matrix{RGB})"
+        precompile(Tuple{typeof(test_reference),File{DataFormat{:PNG},String},Matrix{RGBA{Float64}},Function,Nothing}) || @warn "Failure to precompile ReferenceTests.test_reference(File{format\"PNG\",String},Matrix{RGBA})"
+    else
+        precompile(Tuple{typeof(test_reference),File{DataFormat{:PNG}},Matrix{RGB{Float64}},Function,Nothing})  || @warn "Failure to precompile ReferenceTests.test_reference(File{format\"PNG\"},Matrix{RGB})"
+        precompile(Tuple{typeof(test_reference),File{DataFormat{:PNG}},Matrix{RGBA{Float64}},Function,Nothing}) || @warn "Failure to precompile ReferenceTests.test_reference(File{format\"PNG\"},Matrix{RGBA})"
+    end
 end
 
 end # module
