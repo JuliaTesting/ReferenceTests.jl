4f4f0ed3d7a5be90ca0956fa2cde0b3e853f9134
Mason Protter <mason.protter@icloud.com>
2025-11-08 13:26:36 +0100

    mark ambiguity test as broken on 1.13 (caused by ImageCore)

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,7 +23,7 @@ else
 end
 
 ambs = detect_ambiguities(ReferenceTests, Base, Core)
-@test isempty(ambs)
+@test isempty(ambs) broken=(VERSION>v"1.13.0-DEV.0")
 
 # to properly test world age issues, the full test dependencies must be loaded later
 include("test_no_world_age_issues.jl")
